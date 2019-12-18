ea662bda29815be54803b3f8e2f0f70590c9051d
Joe Gerhard <jlgerhard@gmail.com>
2019-12-17 20:58:13 -0600

    Begin adding passport google auth to server.

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+let user = {};
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+})
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+})
+
+// Google Strategy
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackUrl: process.env.GOOGLE_CALLBACK,
+}, 
+  (accessToken, refreshToken, profile, cb) => {
+    console.log(JSON.stringify(profile))
+    user = { ...profile };
+    return cb(null, profile);
+  }  
+))
+
 const app = express();
 
+app.use(cors());
+app.use(passport.initialize());
+ 
 app.use(logger('dev'));
 app.use(express.json());
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("/auth/google", passport.authenticate("google")); 
+app.get("/oath2callback", passport.authenticate("google"), 
+  (req, res) => {
+  res.redirect("/profile");
+})
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
